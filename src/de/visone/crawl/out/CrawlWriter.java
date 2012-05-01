7fa77fb7c8fdc3b6a2402e3ecbc843d498a2cdc3
Josua Krause <josua.krause@googlemail.com>
2012-05-01 14:24:09 +0200

    no warnings

--- src/de/visone/crawl/out/CrawlWriter.java
+++ src/de/visone/crawl/out/CrawlWriter.java
@@ -11,62 +11,62 @@ import de.visone.crawl.sys.Utils;
 
 public class CrawlWriter implements CrawlListener {
 
-	private final File base;
+    private final File base;
 
-	public CrawlWriter(final File base) {
-		this.base = base;
-	}
+    public CrawlWriter(final File base) {
+        this.base = base;
+    }
 
-	@Override
-	public void pageCrawled(final Content c) {
-		final File f = Utils.toFile(c.getURL(), base, "txt");
-		Utils.ensureDir(f.getParentFile());
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter(f);
-			writeContent(fw, c);
-		} catch (final IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (fw != null) {
-				try {
-					fw.close();
-				} catch (final IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    @Override
+    public void pageCrawled(final Content c) {
+        final File f = Utils.toFile(c.getURL(), base, "txt");
+        Utils.ensureDir(f.getParentFile());
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(f);
+            writeContent(fw, c);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-	private void writeContent(final FileWriter fw, final Content c)
-			throws IOException {
-		fw.append(c.getURL().toString());
-		fw.append(NL);
-		for (final Link l : c.getAcceptedLinks()) {
-			writeLink(fw, l);
-		}
-		for (final Link l : c.getOtherLinks()) {
-			writeLink(fw, l);
-		}
-		final String txt = c.getText();
-		if (!txt.isEmpty()) {
-			fw.append(NL);
-			fw.append(txt);
-		}
-	}
+    private static void writeContent(final FileWriter fw, final Content c)
+            throws IOException {
+        fw.append(c.getURL().toString());
+        fw.append(NL);
+        for (final Link l : c.getAcceptedLinks()) {
+            writeLink(fw, l);
+        }
+        for (final Link l : c.getOtherLinks()) {
+            writeLink(fw, l);
+        }
+        final String txt = c.getText();
+        if (!txt.isEmpty()) {
+            fw.append(NL);
+            fw.append(txt);
+        }
+    }
 
-	private void writeLink(final FileWriter fw, final Link l)
-			throws IOException {
-		fw.append(l.getUrl().toString());
-		for (final String name : l.getText()) {
-			fw.append(' ');
-			fw.append(name);
-		}
-		fw.append(NL);
-	}
+    private static void writeLink(final FileWriter fw, final Link l)
+            throws IOException {
+        fw.append(l.getUrl().toString());
+        for (final String name : l.getText()) {
+            fw.append(' ');
+            fw.append(name);
+        }
+        fw.append(NL);
+    }
 
-	@Override
-	public void close() throws IOException {
-		// nothing to do...
-	}
+    @Override
+    public void close() throws IOException {
+        // nothing to do...
+    }
 }
--- src/de/visone/crawl/sys/CSVReader.java
+++ src/de/visone/crawl/sys/CSVReader.java
@@ -8,101 +8,101 @@ import java.util.Scanner;
 
 public class CSVReader {
 
-	private final Scanner in;
+    private final Scanner in;
 
-	private final char comma;
+    private final char comma;
 
-	private final char quot;
+    private final char quot;
 
-	private String[] nextRow;
+    private String[] nextRow;
 
-	public CSVReader(final File in) throws IOException {
-		this(new Scanner(in));
-	}
+    public CSVReader(final File in) throws IOException {
+        this(new Scanner(in));
+    }
 
-	public CSVReader(final Scanner in) {
-		this(in, ';', '"');
-	}
+    public CSVReader(final Scanner in) {
+        this(in, ';', '"');
+    }
 
-	public CSVReader(final Scanner in, final char comma, final char quot) {
-		this.in = in;
-		this.comma = comma;
-		this.quot = quot;
-		fetchNextRow();
-	}
+    public CSVReader(final Scanner in, final char comma, final char quot) {
+        this.in = in;
+        this.comma = comma;
+        this.quot = quot;
+        fetchNextRow();
+    }
 
-	private StringBuilder addCur(final List<String> list,
-			final StringBuilder cur) {
-		if (cur != null) {
-			list.add(cur.toString());
-		}
-		return new StringBuilder();
-	}
+    private static StringBuilder addCur(final List<String> list,
+            final StringBuilder cur) {
+        if (cur != null) {
+            list.add(cur.toString());
+        }
+        return new StringBuilder();
+    }
 
-	private void fetchNextRow() {
-		if (!in.hasNextLine()) {
-			nextRow = null;
-			return;
-		}
-		final List<String> list = new LinkedList<String>();
-		final StringBuilder row = new StringBuilder();
-		StringBuilder cur = null;
-		int pos = 0;
-		boolean inQuote = false;
-		boolean replacing = false;
-		boolean atStart = true;
-		do {
-			if ((inQuote && !replacing) || atStart) {
-				row.append(in.nextLine());
-			} else {
-				inQuote = false;
-			}
-			while (pos < row.length()) {
-				final char c = row.charAt(pos++);
-				if (atStart) {
-					atStart = false;
-					cur = addCur(list, cur);
-					if (c == quot) {
-						inQuote = true;
-						replacing = false;
-						continue;
-					}
-					inQuote = false;
-				}
-				if (c == comma) {
-					if (!inQuote || replacing) {
-						atStart = true;
-						continue;
-					}
-				}
-				if (inQuote && c == quot) {
-					replacing = !replacing;
-					if (replacing) {
-						continue;
-					}
-				}
-				if (cur != null) {
-					cur.append(c);
-				}
-			}
-			if (atStart) {
-				atStart = false;
-				cur = addCur(list, cur);
-				inQuote = false;
-			}
-		} while (inQuote && in.hasNextLine());
-		addCur(list, cur);
-		nextRow = list.toArray(new String[list.size()]);
-	}
+    private void fetchNextRow() {
+        if (!in.hasNextLine()) {
+            nextRow = null;
+            return;
+        }
+        final List<String> list = new LinkedList<String>();
+        final StringBuilder row = new StringBuilder();
+        StringBuilder cur = null;
+        int pos = 0;
+        boolean inQuote = false;
+        boolean replacing = false;
+        boolean atStart = true;
+        do {
+            if ((inQuote && !replacing) || atStart) {
+                row.append(in.nextLine());
+            } else {
+                inQuote = false;
+            }
+            while (pos < row.length()) {
+                final char c = row.charAt(pos++);
+                if (atStart) {
+                    atStart = false;
+                    cur = addCur(list, cur);
+                    if (c == quot) {
+                        inQuote = true;
+                        replacing = false;
+                        continue;
+                    }
+                    inQuote = false;
+                }
+                if (c == comma) {
+                    if (!inQuote || replacing) {
+                        atStart = true;
+                        continue;
+                    }
+                }
+                if (inQuote && c == quot) {
+                    replacing = !replacing;
+                    if (replacing) {
+                        continue;
+                    }
+                }
+                if (cur != null) {
+                    cur.append(c);
+                }
+            }
+            if (atStart) {
+                atStart = false;
+                cur = addCur(list, cur);
+                inQuote = false;
+            }
+        } while (inQuote && in.hasNextLine());
+        addCur(list, cur);
+        nextRow = list.toArray(new String[list.size()]);
+    }
 
-	public boolean hasNextRow() {
-		return nextRow != null;
-	}
+    public boolean hasNextRow() {
+        return nextRow != null;
+    }
 
-	public String[] getNextRow() {
-		final String[] res = nextRow;
-		fetchNextRow();
-		return res;
-	}
+    public String[] getNextRow() {
+        final String[] res = nextRow;
+        fetchNextRow();
+        return res;
+    }
 
 }
